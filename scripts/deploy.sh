f893abad223ef1feb9af6f58577cefac6dd0e683
jaewoo2233 <ta3844@naver.com>
2023-05-25 21:19:09 +0900

    HOTFIX: 쉘스크립트 에러 해결중

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,16 @@
+#!/bin/bash
 
 # JAR 파일 경로
 jar_path="/home/ubuntu/deploy/deploy/back-0.0.1-SNAPSHOT.jar"
 
-# 실행 중인 프로세스 종료
-pkill -f "$jar_path"
+# 실행 중인 프로세스 ID 확인
+pid=$(pgrep -f "$jar_path")
 
-# 스프링 서버 다시 실행
-nohup java -jar "$jar_path" &
+if [ -n "$pid" ]; then
+  echo "Stopping the Spring server..."
+  kill "$pid"
+  sleep 5
+fi
+
+echo "Starting the Spring server..."
+nohup java -jar "$jar_path"
