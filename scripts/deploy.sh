dc5851c86c44a4bd25b55fcc1d6dee9c58ff98fa
jaewoo2233 <ta3844@naver.com>
2023-05-25 20:58:55 +0900

    CONFIG: 깃액션 테스트 10

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,9 @@
-#!/bin/bash
-BUILD_JAR=$(ls /home/ubuntu/deploy/deploy/back-0.0.1-SNAPSHOT.jar)
-nohup java -jar "$BUILD_JAR" >> /home/ubuntu/deploy_test.log 2>/home/ubuntu/action/deploy_err_test.log &
+
+# JAR 파일 경로
+jar_path="/home/ubuntu/deploy/deploy/back-0.0.1-SNAPSHOT.jar"
+
+# 실행 중인 프로세스 종료
+pkill -f "$jar_path"
+
+# 스프링 서버 다시 실행
+nohup java -jar "$jar_path" >> /home/ubuntu/deploy_test.log 2>/home/ubuntu/action/deploy_err_test.log &
