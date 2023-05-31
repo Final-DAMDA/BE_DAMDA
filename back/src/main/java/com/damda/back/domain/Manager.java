439c7f2ff6ea0180f806f28493520dab9fa9b490
jerok <jerok.kim@gmail.com>
2023-05-31 17:06:46 +0900

    FEAT: update Manager domain - #10

--- back/src/main/java/com/damda/back/domain/Manager.java
+++ back/src/main/java/com/damda/back/domain/Manager.java
@@ -5,33 +5,47 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.awt.geom.Area;
 import java.time.DayOfWeek;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Getter
+@Table(name = "manager_tb")
 public class Manager {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    
-    private Integer userId;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "member_id", referencedColumnName = "id")
+    @Column(nullable = false)
+    private Member userId;
     
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private DayOfWeek activityDate;
     
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Area activityArea;
     
-    private String certificateStatus;
-    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CertificateStatus certificateStatus;
+
+    @Column(nullable = false)
     private boolean vehicle;
-    
+
+    @Column(nullable = false)
     private boolean fieldExperience;
-    
+
+    @Column(nullable = false)
     private boolean serviceRule;
     
 }
+
+enum Area {}
+
+enum CertificateStatus {}
