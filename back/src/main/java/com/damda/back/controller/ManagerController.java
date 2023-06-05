70ce4f5f749a90006842168384bd31652943469c
jerok <jerok.kim@gmail.com>
2023-06-05 17:24:45 +0900

    FEAT: create ManagerController - #10

---
+++ back/src/main/java/com/damda/back/controller/ManagerController.java
@@ -0,0 +1,32 @@
+package com.damda.back.controller;
+
+import com.damda.back.data.common.CodeEnum;
+import com.damda.back.data.common.CommonResponse;
+import com.damda.back.data.request.ManagerApplicationDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ManagerController {
+
+    @PostMapping("/api/v1/member/manager")
+    public ResponseEntity<CommonResponse<?>> managerCreate(@RequestBody ManagerApplicationDTO managerApplicationDTO) {
+
+        CommonResponse<?> commonResponse = CommonResponse
+                .builder()
+                .codeEnum(CodeEnum.SUCCESS)
+                .data("")
+                .build();
+
+        return ResponseEntity
+                .status(commonResponse.getStatus())
+                .body(commonResponse);
+    }
+
+}
