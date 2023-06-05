d3450cff1885ee35bad55bb93f59aa85832c66b2
jerok <jerok.kim@gmail.com>
2023-06-05 17:31:40 +0900

    FEAT: create manager service - #10

---
+++ back/src/main/java/com/damda/back/service/Impl/ManagerServiceImpl.java
@@ -0,0 +1,32 @@
+package com.damda.back.service.Impl;
+
+import com.damda.back.data.request.ManagerApplicationDTO;
+import com.damda.back.repository.ManagerRepository;
+import com.damda.back.repository.MemberRepository;
+import com.damda.back.service.ManagerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ManagerServiceImpl implements ManagerService {
+    
+    private final MemberRepository memberRepository;
+    
+    private final ManagerRepository managerRepository;
+    
+    // TODO:
+    // - area repository
+    // - regionManager repository
+    // - activityDay repository
+    
+    @Override
+    @Transactional
+    public boolean managerCreate(ManagerApplicationDTO dto, Integer memberId) {
+        
+        
+        
+        return false;
+    }
+}
