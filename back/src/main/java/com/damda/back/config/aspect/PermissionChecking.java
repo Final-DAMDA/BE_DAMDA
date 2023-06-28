85a3149f563662e791cc59b576c1a4a849734e8b
jaewoo2233 <ta3844@naver.com>
2023-06-28 10:03:54 +0900

    FEAT: 쿠키 HttpOnly변환

--- back/src/main/java/com/damda/back/config/aspect/PermissionChecking.java
+++ back/src/main/java/com/damda/back/config/aspect/PermissionChecking.java
@@ -23,28 +23,32 @@ import javax.servlet.http.HttpServletResponse;
 @Component
 public class PermissionChecking {
 
-    @Pointcut("@annotation(com.damda.back.config.annotation.Permission)")
-    private void enablePermission() {}
+    @Pointcut("@annotation(com.damda.back.config.annotation.AdminBlocking)")
+    private void adminBlocking() {}
+
+    @Pointcut("@annotation(com.damda.back.config.annotation.AdminBlocking)")
+    private void userBlocking() {}
 
 
 
-    @Around("enablePermission()")
-    public Object around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+    /**
+     * @apiNote 권한이 어드민이 아닐 경우 통과 어드민이면 막힘
+     * */
+    @Around("adminBlocking()")
+    public Object aroundAdminBlocking(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         HttpServletRequest request =
                 ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
 
         Object temp = request.getAttribute("role");
         String role = null;
-
         if(temp != null) role = temp.toString();
 
         log.info(" 해당 사용자 권한 ===> {}",role);
-        if(role != null && (role.equals("ADMIN"))){
+        if(role != null && (!role.equals("ADMIN"))){
             return proceedingJoinPoint.proceed();
         }else {
             HttpServletResponse response =
                     ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getResponse();
-            //RequestContextHolder를 사용하면 현재 쓰레드에서 처리되고 있는 요청에 관한 HttpServletRequest와 HttpServletResponse를 가져올 수 있음
             Gson gson = new Gson();
 
             CommonResponse<?> commonResponse = CommonResponse.builder()
@@ -52,10 +56,41 @@ public class PermissionChecking {
                     .build();
             String json = gson.toJson(commonResponse);
 
-            log.warn("Attempt to access resources without permission {}", role);
+            log.warn("권한체크 포인트를 잘못요청함 {}", role);
 
             return ResponseEntity.status(403).body(json);
         }
+    }
+
+
+    @Around("userBlocking()")
+    public Object aroundUserBlocking(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
+        HttpServletRequest request =
+                ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+
+        Object temp = request.getAttribute("role");
+        String role = null;
+        if(temp != null) role = temp.toString();
+
+        log.info(" 해당 사용자 권한 ===> {}",role);
+        if(role != null && (!role.equals("USER"))){
+            return proceedingJoinPoint.proceed();
+        }else {
+            HttpServletResponse response =
+                    ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getResponse();
+            Gson gson = new Gson();
+
+            CommonResponse<?> commonResponse = CommonResponse.builder()
+                    .codeEnum(CodeEnum.NO_PERMISSIONS)
+                    .build();
+            String json = gson.toJson(commonResponse);
+
+            log.warn("권한체크 포인트를 잘못요청함 {}", role);
+
+            return ResponseEntity.status(403).body(json);
+        }
+
+
     }
 
 }
